3bdba1745481bfa4ffd06f7fb46c3db66daecee4
Erlend Golten Persen <erlendgp@stud.ntnu.no>
2023-03-20 19:53:01 +0100

    Lagt til insert databser greie

--- main.py
+++ main.py
@@ -3,52 +3,38 @@ con = sqlite3.connect("database.db")
 
 #Create cursor
 c = con.cursor()
-c.execute("SELECT * FROM sqlite_master")
 
-# #Banestrekning
-# c.execute('''CREATE TABLE Banestrekning
-# (BaneNavn TEXT, Fremdriftsenergi INTEGER, StartStasjon TEXT,
-# EndeStasjon TEXT, DelSNavn TEXT,
-# CONSTRAINT Banestreking_PK PRIMARY KEY (BaneNavn),
-# CONSTRAINT Banestrekning_FK FOREIGN KEY (DelSNavn)
-#         ON UPDATE CASCADE,
-#         ON DELETE CASCADE
-# )''')
+c.execute('''INSERT OR IGNORE INTO Jernbanestasjon VALUES 
+('Trondheim', 5.1)
+''')
+c.execute('''INSERT OR IGNORE INTO Delstrekning VALUES 
+('Trond_Stein', 120, 2, 'Trondheim', 'Steinkjaer')
+''')
+c.execute('''INSERT OR IGNORE INTO Banestrekning VALUES 
+('Nordlandsbanen', 0, 'Trondheim', 'Bodoe', 'Trond_Stein')
+''')
+c.execute('''INSERT OR IGNORE INTO Togrute VALUES 
+('1', 0, 'Trondheim', 'Bodoe', 'Trond_Stein')
+''')
 
-# #Delstrekning
-# c.execute('''CREATE TABLE Delstrekning
-# (DelSNavn TEXT, LengdeIKm INTEGER, AntallSpor INTEGER NOT NULL,
-# StartStasjon TEXT, EndeStasjon TEXT
-# CONSTRAINT Delstrekning_PK PRIMARY KEY (DelSNavn)
-# )''')
 
-# #Jernbanestasjon
-# c.execute('''CREATE TABLE Jernbanestasjon
-# (StasjonNavn TEXT, moh INTEGER
-# CONSTRAINT Jernbanestasjon_PK PRIMARY KEY (StasjonNavn)
-# )''')
+
+
           
-# #Togrute         
-# c.execute('''CREATE TABLE Togrute
-# (RuteID INTEGER NOT NULL, Retning INTEGER NOT NULL, BaneNavn TEXT,
-# OperatorNavn TEXT
-# )''')
-          
-# #TogruteForekomst          
-# c.execute('''CREATE TABLE TogruteForekomst
-# (TogruteForekomstID INTEGER NOT NULL, OperatorNavn TEXT
-# CONSTRAINT TogruteForekomst_PK PRIMARY KEY (TogruteForekomstID)
-# )''')
-          
-
-
-#Fecthall
-
-
-
 c.execute("SELECT * FROM Jernbanestasjon")
+c.execute("SELECT * FROM Delstrekning")
+c.execute("SELECT * FROM Banestrekning")
 rows = c.fetchall()
-print("All rows in the table Jernbasestasjon:")
 print(rows)
+
 con.commit()
 con.close()
+
+
+
+# c.execute("SELECT * FROM Jernbanestasjon")
+# rows = c.fetchall()
+# print("All rows in the table Jernbasestasjon:")
+# print(rows)
+# con.commit()
+# con.close()
