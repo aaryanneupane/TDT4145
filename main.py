35926f4f84f694a3727e250a8b9a209406289a76
Erlend Golten Persen <erlendgp@stud.ntnu.no>
2023-03-20 20:21:09 +0100

    lagt til data til main.py

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ con = sqlite3.connect("database.db")
 c = con.cursor()
 
 c.execute('''INSERT OR IGNORE INTO Jernbanestasjon VALUES 
-('Trondheim', 5.1)
+('Trondheim', 5.1),
+('Steinkjer', 3.6),
+('Mosjøen', 6.8),
+('MoIRana', 3.5),
+('Fauske', 34.0),
+('Bodø', 4.1),
 ''')
 c.execute('''INSERT OR IGNORE INTO Delstrekning VALUES 
-('Trond_Stein', 120, 2, 'Trondheim', 'Steinkjaer')
+('TrondStein', 120, 2, 'Trondheim', 'Steinkjer')
+('SteinMosjø', 280, 1, 'Steinkjer', 'Mosjøen')
+('MosjøMoIRa', 90, 1, 'Mosjøen', 'MoIRana')
+('MoIRaFaus', 170, 1, 'MoIRana', 'Fauske')
+('FausBod', 60, 1, 'Fauske', 'Bodø')
 ''')
 c.execute('''INSERT OR IGNORE INTO Banestrekning VALUES 
-('Nordlandsbanen', 0, 'Trondheim', 'Bodoe', 'Trond_Stein')
+('Nordlandsbanen', 0, 'Trondheim', 'Bodoe', 'TrondStein')
 ''')
 c.execute('''INSERT OR IGNORE INTO Togrute VALUES 
 ('1', 0, 'Trondheim', 'Bodoe', 'Trond_Stein')
@@ -20,9 +29,8 @@ c.execute('''INSERT OR IGNORE INTO Togrute VALUES
 
 
 
-          
-c.execute("SELECT * FROM Jernbanestasjon")
-c.execute("SELECT * FROM Delstrekning")
+
+        
 c.execute("SELECT * FROM Banestrekning")
 rows = c.fetchall()
 print(rows)
